2ad865fb3eec14a2926741f6a07cf1c4c9ba23dd
gastonemanuel1997@hotmail.com <gastonemanuel1997@hotmail.com>
2023-03-25 21:35:25 -0300

    deploy back

--- api/src/db.js
+++ api/src/db.js
@@ -6,10 +6,23 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
+
+let sequelize =
+  process.env.NODE_ENV === "production"
+    ? new Sequelize(DATABASE_URL, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-});
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
+}): new Sequelize(
+        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`,
+        { logging: false, native: false }
+      );
+      
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
